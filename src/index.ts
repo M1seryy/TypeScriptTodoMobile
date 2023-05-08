80fbd30a1f842e937dc54b9eaad39c9a095830d9
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-05-08 13:29:07 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,65 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
+const list = document.querySelector<HTMLUListElement>('.tasks');
+const input = document.querySelector<HTMLInputElement>('#input');
+const form = document.querySelector<HTMLFormElement>('.new-task-form') || null;
+let index: number = 0;
 
-import confetti from 'canvas-confetti';
+type TaskType = {
+  id: string;
+  title: string;
+  complete: boolean;
+  createdAt: Date;
+};
+const tasks: TaskType[] = createMarkup();
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+tasks.forEach(createTask);
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input?.value == '' || input?.value == null) {
+    return;
+  }
+  const task: TaskType = {
+    id: uuidV4(),
+    title: input.value,
+    complete: false,
+    createdAt: new Date(),
+  };
+  createTask(task);
+  input.value = '';
+  tasks.push(task);
+  saveTasks();
+});
+
+function createTask(task: TaskType) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  index += 1;
+  label.className = 'label-' + index;
+  const textTag = document.createElement('h2');
+  textTag.textContent = task.title;
+  const checkBox = document.createElement('input');
+  checkBox.id = 'label-' + index;
+  label.htmlFor = checkBox.id;
+  checkBox.type = 'checkbox';
+  checkBox.checked = task.complete;
+  label.append(textTag);
+  item.append(checkBox, label);
+  list?.append(item);
+}
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function createMarkup(): TaskType[] {
+  const jsonTask = localStorage.getItem('TASKS');
+  if (jsonTask == null) {
+    return [];
+  }
+  return JSON.parse(jsonTask);
+}
+const clearBtn = document.getElementById('clear') as HTMLButtonElement;
+clearBtn.addEventListener('click', clearAllTaks);
+function clearAllTaks() {
+  list?.remove()
+  localStorage.clear();
+}
